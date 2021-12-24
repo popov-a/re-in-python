01ef4f910e97c831a0d0151901700c656ad8ae6c
bio-ton <popov_a_v@mail.ru>
2021-12-24 15:56:59 +0400

    some examples added

--- main.py
+++ main.py
@@ -27,12 +27,41 @@ $       окончание строки
 '''
 import re
 
-# re.match('AR', 'aARaAR') ищет вхождение в начале строки
-# re.search('AR', 'aARaAR') ищет первое совпадение в любом месте строки
-# re.findall('AR', 'aARaAR') ищет все совпадения в строке
-# re.split()
-# re.sub()
-# re.compile()
+# re.match(шаблон, строка)          ищет вхождение шаблона в начале строки
+# re.search(шаблон, строка)         ищет первое совпадение с шаблоном в любом месте строки
+# re.findall(шаблон, строка)        ищет все совпадения с шаблоном в строке
+
+# re.finditer(шаблон, строка)       ищет все совпадения с шаблоном в строке и возвращает итерируемый объект,
+#                                   у которого доступно начало и конец вхождения: m.group(), m.start(), m.end()
+
+# re_compiled = re.compile(шаблон)  компилирует шаблон, в дальнейшем можно использовать re_compiled.findall(строка)
+
+# re.split(шаблон, строка, maxsplit=0)          разбивает строку используя в качестве разеделителя шаблон
+#                                               пример:     re.split(r'[ ,\[\]]+', my_string)
+#                                               разбиваем строку my_string по пробелам, запятым и кватратным скобкам
+
+# re.sub(шаблон, замена, строка, count=0, flags=0)      замена в строке найденных шаблонов на "замена"
+#                                                       пример:     re.sub(r'\b\w+', 'word', 'My name is Ivan', count=3)
+#                                                       заменить первые три слова на "word"
+
+# Пример поиска и использования отдельных шаблонов в строке
+# mac_table = '''
+#     100    aabb.cc10.7000    DYNAMIC     Gi0/1
+#     200    aabb.cc20.7000    DYNAMIC     Gi0/2
+# '''
+
+# print(re.sub(r' *(\d+) +'
+#             r'([a-f0-9]+)\.'
+#             r'([a-f0-9]+)\.'
+#             r'([a-f0-9]+) +\w+ +'
+#             r'(\S+)',
+#             r'\1 \2:\3:\4 \5',
+#             mac_table))
+
+# 100 aabb:cc10:7000 Gi0/1
+# 200 aabb:cc20:7000 Gi0/2
+
+
 if __name__ == '__main__':
     s = 'a aA aa user@mail.ru root@example.com 2021-01-02 2021-01-03 Андрей Семен Василий Олег апельсин'
     print('Строка для исследования:')
@@ -81,3 +110,56 @@ if __name__ == '__main__':
     # извлечь слова, начинающиеся на гласную
     print(re.findall(r'\b[уеыаоэяиюУЕЫАОЭЯИЮ]\w*', s))
 
+    # итерация по результатм поиска, кроме кортежа найденных выражений мы получаем начало и конец вхождения
+    for match in re.finditer(r'\b[уеыаоэяиюУЕЫАОЭЯИЮ]\w*', s):
+        print(match.group(), match.start(), match.end())
+
+    # заменить первые 3 слова на слово "word"
+    print(re.sub(r'\b\w+', 'word', 'My name is Ivan', count=3))
+    # заменить первые все слова начинающиеся на букву 'i' (без учета регистра) на слово "word"
+    print(re.sub(r'\bi\w*', 'word', 'My name is Ivan', flags=re.IGNORECASE))
+
+    # все слова на букву b не зависимо от регистра
+    text = """ab Betty bought a bit of butter, But the butter was so bitter, So she bought some better butter, To make the bitter butter better."""
+    print(re.findall(r'\bb\w*', text, flags=re.IGNORECASE))
+
+    # удалить все знаки препинания
+    sentence = """A, very very; irregular_sentence"""
+    print(' '.join(re.findall(r'[A-Za-z]+', sentence)))
+    print(' '.join(re.split(r'[,;_ ]+', sentence)))
+
+    # очистить текст от всякой служебной хрени
+    tweet = '''Good advice! RT @TheNextWeb: What I would do differently if I was learning to code today https://t.co/lbwej0pxOd cc: @garybernhardt #rstats'''
+
+
+    def clean_tweet(tweet: str) -> str:
+        tweet = re.sub(r'rt|cc', '', tweet, flags=re.IGNORECASE)
+        tweet = re.sub(r'@\S+', '', tweet)
+        tweet = re.sub(r'#\S+', '', tweet)
+        tweet = re.sub(r'https\S+', '', tweet, flags=re.IGNORECASE)
+        tweet = re.sub(r'\s+', ' ', tweet)
+        return tweet
+
+
+    print(clean_tweet(tweet))
+
+    # извлечь текст между тегами
+    html = '''
+    <HTML>
+    <HEAD>
+    <TITLE>Your Title Here</TITLE>
+    </HEAD>
+    
+    <BODY>
+    <HR>
+    <a href="http://someurl.com">Link Name</a>
+    <H1>This is a Header</H1>
+    <H2>This is a Medium Header</H2>
+    <P>This is a new paragraph! </P>
+    <P>This is a another paragraph!</P>
+    <B>This is a new sentence without a paragraph break, in bold italics.</B>
+    <HR>
+    </BODY>
+    </HTML>
+    '''
+    print(re.findall(r'<.+?>(.+)<.+?>', html))
